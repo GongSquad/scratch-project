7d890929a8948ea51a7eee34bbf4d337ffb93ecb
tehyaarassman <tehyaarassman@gmail.com>
2021-11-20 10:41:09 -0600

    more redux setup

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,3 +1,19 @@
 import * as types from '../constants/actionTypes';
+// normal redux action creator for creating an entry
+// export const addEntryActionCreator = (entryID) => {
+//   return {
+//     type: types.ADD_ENTRY,
+//     payload: entryID
+//   }
+// }
 
-//export const addEntryActionCreator = ()
+// redux thunk version-->
+
+
+
+// for getting/ fetching the posts from the database
+// export const fetchPosts = () => {
+//   return {
+//       type: types.FETCH_POSTS
+//   };
+// };
---
+++ client/constants/actionTypes.js
@@ -0,0 +1,4 @@
+//create constants to protect against spelling errors
+
+export const ADD_ENTRY = 'ADD_ENTRY';
+export const FETCH_POSTS = 'FETCH_POSTS'
