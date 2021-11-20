bf41754632efc5994efe1825d97a22d8fd5f6b57
Daniel <dannguyen1191@yahoo.com>
2021-11-20 12:26:39 -0600

    post route WIP

--- server/controllers/EntryController.js
+++ server/controllers/EntryController.js
@@ -1,2 +1,26 @@
-const Schema = require('../models/EntryModel.js');
+const Entry = require('../models/EntryModel.js');
 
+const EntryController = {
+  createEntry(req, res) {
+    const { title, date, category, text } = req.body;
+    if (!date || !category) return res.render('Missing date and/or category from EntryController.createEntry');
+  
+    Entry.create({ title: title, date: date, category: category, text: test }, (err, entry) => {
+      if (err) {
+        console.log('MONGOERRORSAYSWHATTTTTT', err);
+        res.status(400);
+      } else{
+        // console.log(student);
+        console.log('connected to DB')
+        res.locals.entry = entry;
+        res.json(res.locals.entry);
+
+        // next();
+        // res.send({student: res.locals.student});
+      }
+    });
+  },
+};
+
+
+module.exports = EntryController;
